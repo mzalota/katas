76e3a2fafb2b39741fdcd9d714b05911b2493c72
zal0001m <maxim.zalota@prosiebensat1.com>
2024-01-28 16:32:49 +0100

    Improved katas for "Extract common part from 'if'" Intention

--- Katas.java
+++ Katas.java
@@ -1,6 +1,59 @@
 public class Katas {
 
 
+    //1) Intention: "Extract common part from 'if'"
+    //2) Intention: "Move up into if statement branches"
+    public void ifExtractCommonPart (boolean argumentOne) {
+        if (argumentOne) {
+            barMethod("xxx");
+            System.out.println("Message - Done");
+        } else {
+            fooMethod("yyy");
+            System.out.println("Message - Done");
+        }
+    }
+
+
+    //--- If there are multiple common statements, IntelliJ gets confused and does not offer Intention "Extract common part from 'if'".
+    //Use a trick - extract these lines temporarily into a function.
+
+    //1) Select two lines with barMethod("xxx"); and fooMethod("yyy");. Extract Method (Ctrl+Alt+M). Both sections should be replaced by new method "extracted"
+    //2) Intention: "Extract common part from 'if'" -> "extracted()" method moved to below if-else block
+    //3) Refactoring: Inline extracted() method (Ctrl+Alt+M)
+    public void ifExtractCommonPartMultipleStatements (boolean argumentOne) {
+        if (argumentOne) {
+            System.out.println("If ACTION");
+            barMethod("xxx");
+            fooMethod("yyy");
+        } else {
+            System.out.println("Else ACTION");
+            barMethod("xxx");
+            fooMethod("yyy");
+        }
+    }
+
+    //1) Intention: "Extract common part from 'if'(may change semantics)" -> barMethod("xxx") is extracted above if.
+    //2a) Select "Message 111" String and extract it into a variable with name String (Ctrl+Alt+V refactoring)
+    //2b) Select "Message 222" String and extract it into a variable with name String (Ctrl+Alt+V refactoring)
+    //3) Intention: "Split into declaration and assignment" on both message variables
+    //4) Move manually (Ctrl+Shift+Up/Down) "String message;" declaration from within if block to above it.
+    //5) Click on second declaration of String message; that now is in Error state
+    //6) Intention: "Reuse previous variable 'message' declaration"
+    //7) Intention: "Extract common part from 'if'" -> "fooMethod(message)" method moved to below if-else block
+    public void ifExtractCommonPartBelowAndAbove (boolean argumentOne) {
+        if (argumentOne) {
+            barMethod("xxx");
+            System.out.println("If ACTION");
+            fooMethod("Message 111");
+        } else {
+            barMethod("xxx");
+            System.out.println("Else ACTION");
+            fooMethod("Message 222");
+        }
+    }
+
+
+
     // 1) Intention: "Invert if condition"
     // Repeat from start -> Intention: "Transform body to single exit-point form"
     public void ifStatmentAtFunctionEnd(boolean firstArgument, boolean secondArgument) {
@@ -62,33 +115,14 @@ public class Katas {
         }
     }
 
-    //1) Intention: "Extract common part from 'if'"
-    //2) Intention: "Move up into if statement branches"
-    public void ifExtractCommon (boolean argumentOne) {
-        String message;
-        if (argumentOne) {
-            message = "Message 111";
-            System.out.println("If ACTION");
-        } else {
-            message = "Message 222";
-            System.out.println("Else ACTION");
-        }
+
+
+
+    private void fooMethod(String message) {
+        System.out.println(message);
+    }
+    private void barMethod(String message) {
         System.out.println(message);
     }
 
-    //1) Comment out one line with "Top message" string (otherwise IntelliJ is too confused)
-    //2) Intention: "Extract common part from 'if'" -> "Bottom message" is extracted behind if
-    //3) Uncomment out one line from 1)
-    //4) Intention: "Extract common part from 'if'(may change semantics)" -> "Top message" is extracted above if
-    public void ifExtractCommon2 (boolean argumentOne) {
-        if (argumentOne) {
-            System.out.println("Top message");
-            System.out.println("If ACTION");
-            System.out.println("Bottom message");
-        } else {
-            System.out.println("Top message");
-            System.out.println("Else ACTION");
-            System.out.println("Bottom message");
-        }
-    }
 }
