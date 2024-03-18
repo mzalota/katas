863da36fe6527ba163423819b27f8e21c8af87b4
zal0001m <maxim.zalota@prosiebensat1.com>
2024-03-18 08:50:07 +0100

    Refactoring flow: Push logic up-stack/down-stack

--- com/katas/FunctionParams.java
+++ com/katas/FunctionParams.java
@@ -2,18 +2,26 @@ package com.katas;
 
 public class FunctionParams {
 
+    // Refactoring flows: "Push logic down-stack" and "Pull logic up-stack"
+    //---Push logic "down-stack" (to the function below)
+    //1) Refactor: "Extract Method": lookupPriceInDBNew() - content- last two lines of the method. Choose option "Inline this only and keep method". In "Process Duplicates" select Replace
+    //2) Refactor: "Inline Method...": lookupPriceInDB(). Select first option: "Inline all and remove the method"
+    //3) Refactor: "Rename..." lookupPriceInDBNew() to lookupPriceInDB()
 
-    //---Push logic into subfunction
-
-    //1) Refactoring: "Extract Method": extractedAAA() - content- last two lines of the method. Choose option "Inline this only and keep method". In "Process Duplicates" select Replace
-    //2) Refactoring: "Inline Method...": lookupPriceInDB(). Select first option: "Inline all and remove the method"
-    //3) Refactoring: "Rename..." extractedAAA() to lookupPriceInDB()
+    //--- Pull logic "up-stack" (to the function "above")
+    //1) Refactor: "Extract Method": lookupPriceInDB() - content last line of the method.
+    //2) Refactor: "Inline Method...": lookupPriceInDB(). Select first option: "Inline all and remove the method"
+    //3) Refactor: "Rename..." lookupPriceInDBNew() to lookupPriceInDB()
     public int getBaselinePrice(String priceGroupId, int tarifCategory) {
         int baselineMonth = 01;
         int baselineYear = 2001;
 
         int priceGroupIdInt = Integer.parseInt(priceGroupId);
-        return lookupPriceInDB(baselineMonth, baselineYear, tarifCategory, priceGroupIdInt);
+        return lookupPriceInDB(tarifCategory, baselineMonth, baselineYear, priceGroupIdInt);
+    }
+
+    private static int lookupPriceInDB(int tarifCategory, int baselineMonth, int baselineYear, int priceGroupIdInt) {
+        return priceGroupIdInt * tarifCategory + baselineYear / baselineMonth;
     }
 
 
@@ -26,23 +34,17 @@ public class FunctionParams {
     //-- clean up PriceGroupId class a bit: get rid of unused getPriceGroupId() method
     //4) Refactoring: "Inline Method": PriceGroupId.getPriceGroupId()
 
-    //-- push PriceGroupId object as parameter to lookupPriceInDB() method, instead of Int
-    //5) Refactoring: "Extract Method": tmpMethod() for code "lookupPriceInDB(month, year, tarifCategory, priceGroupId1.getAsInt())"
-    //6) Refactoring: "Inline Method...": lookupPriceInDB()
-    //7) Refactoring: "Rename..." tmpMethod() to lookupPriceInDB()
+    //-- push PriceGroupId object as parameter to lookupPriceInDB() method, instead of Int (Use "Push logic down-stack" Refactoring Flowout)
     public int getPrice(String priceGroupId, int tarifCategory, String date) {
         int month = Integer.parseInt(date.substring(3,5));
         int year = Integer.parseInt(date.substring(6,10));
         try {
-            int priceGroupInt = Integer.parseInt(priceGroupId);
-            return lookupPriceInDB(month, year, tarifCategory, priceGroupInt);
+            int priceGroupIdInt = Integer.parseInt(priceGroupId);
+            return lookupPriceInDB(tarifCategory, month, year, priceGroupIdInt);
         } catch (NumberFormatException e) {
             System.out.println("Error reading from DB");
             return 0;
         }
-    }
-    protected int lookupPriceInDB(int month, int year, int tarifCategory, int priceGroupId) {
-        return priceGroupId*tarifCategory+year/month;
     }
 
 }
