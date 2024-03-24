05d9cf6dd9660cf4f6d2c2b4f4eeadb98e46fca6
zal0001m <maxim.zalota@prosiebensat1.com>
2024-03-24 16:58:24 +0100

    Now lookupPriceInDB() and readPrices() are kind of ready.

--- com/katas/DeDuplicate.java
+++ com/katas/DeDuplicate.java
@@ -25,6 +25,55 @@ public class DeDuplicate {
     private static Logger logger = Logger.getLogger(DeDuplicate.class.getName());
 
 
+    /*
+    1) Right-click on DeDuplicat.java tab on top and select "Split Right". Align lookupPriceInDB() and readPrices() functions. You can jump between them with "Ctrl+Tab" shortcut
+    2) Try Extract Method on last two lines from lookupPriceInDB() to see if the middle two lines in readPrices would be detected.
+    3) Undo, because IntelliJ did not detect duplication.
+    4) In lookupPriceInDB apply "Extract Variable" refactoring to the last line (or use Intention: introduce local variable on "return" statement)
+    5) In readPrices() move String jdbcConnectionForPrice varible declaration to the top of the function.
+    6) You see that now only the 2 places with log statements differ. Copy/paste these log statements to make them identical in each function
+    7) Select all statements in the body of one function and
+     */
+
+    private int lookupPriceInDB(int tarifCategory, int validitMonth, int validityYear, int priceGroupIdInt) {
+
+        String jdbcConnectionForPrice = jdbcConnection+";param1=value1";
+        logger.log (Level.INFO, "Starting to read Price from DB, conn: "+jdbcConnectionForPrice);
+
+        String queryStr = queryBuilder(tarifCategory, validitMonth, validityYear, priceGroupIdInt);
+        return executeQuery(jdbcConnectionForPrice, queryStr);
+    }
+
+    private static String queryBuilder(int year, int month, int tarifCategoryId, int priceGroupId) {
+        return "SELECT column FROM db.table";
+    }
+
+    private int readPrices(int year, int month, int tarifCategoryId, int priceGroupId) {
+        logger.log (Level.INFO, "Starting to read Price from DB");
+
+        String connStr = jdbcConnection+";param1=value1";
+        logger.log (Level.INFO, "DB connection is: "+connStr);
+
+        String sql = queryBuilder(year, month, tarifCategoryId, priceGroupId);
+
+        int responseFromDB = executeQuery(connStr, sql);
+
+        logger.log (Level.INFO, "Finishing reading Price from DB. Response is: "+responseFromDB);
+        return responseFromDB;
+    }
+
+    private static String queryBuilder1(int tarifCategory, int validitMonth, int validityYear, int priceGroupIdInt) {
+        String queryStr = "";
+        queryStr += "SELECT price FROM price_table WHERE";
+        queryStr += " ";
+        queryStr += "price_group_id = "+ priceGroupIdInt;
+        queryStr += " AND ";
+        queryStr += "category_id = "+ tarifCategory;
+        queryStr += " AND ";
+        queryStr += "validity_month = '"+ validityYear +"-"+ validitMonth +"'";
+        return queryStr;
+    }
+
     private int lookupPriceInDBOrig(int tarifCategory, int baselineMonth, int baselineYear, int priceGroupIdInt) {
 
         String jdbcConnectionForPrice = jdbcConnection+";param1=value1";
@@ -36,54 +85,27 @@ public class DeDuplicate {
         return responseFromDB;
     }
 
-    private int lookupPriceInDB(int tarifCategory, int validitMonth, int validityYear, int priceGroupIdInt) {
-
-        String jdbcConnectionForPrice = jdbcConnection+";param1=value1";
-        logger.log (Level.INFO, "Starting to read Price from DB, conn: "+jdbcConnectionForPrice);
-
-        String queryStr = "";
-        queryStr += "SELECT price FROM price_table WHERE";
-        queryStr += " ";
-        queryStr += "price_group_id = "+priceGroupIdInt;
-        queryStr += " AND ";
-        queryStr += "category_id = "+tarifCategory;
-        queryStr += " AND ";
-        queryStr += "validity_month = '"+validityYear+"-"+validitMonth+"'";
-        queryStr += ",";
-
-        return executeQuery(jdbcConnectionForPrice, queryStr);
-    }
-
     public double getBaselinePrice(Integer priceGroupId, int tarifCategory) {
         int baselineMonth = 01;
         int baselineYear = 2001;
 
         int price = lookupPriceInDB(tarifCategory, baselineMonth, baselineYear, priceGroupId);
         if (price == 0) {
-            return loadPrices(baselineYear, baselineMonth, tarifCategory, priceGroupId);
+            return readPrices(baselineYear, baselineMonth, tarifCategory, priceGroupId);
         }
         return price;
     }
 
-    private int loadPrices(int year, int month, int tarifCategoryId, int priceGroupId) {
 
-        logger.log (Level.INFO, "Starting to read Price from DB");
-        String jdbcConnectionForPrice = jdbcConnection+";param1=value1";
-        logger.log (Level.INFO, "DB connection is: "+jdbcConnectionForPrice);
-
+    private static String queryBuilder2(int year, int month, int tarifCategoryId, int priceGroupId) {
         String selectedFields = "price";
         String tableName = "price_table";
         String whereClause = "";
-        whereClause += "price_group_id = '"+priceGroupId+"'  AND ";
-        whereClause += "category_id = '"+tarifCategoryId+"'  AND ";
-        whereClause += "validity_month = '"+year+"-"+month+"';";
+        whereClause += "price_group_id = '"+ priceGroupId +"'  AND ";
+        whereClause += "category_id = '"+ tarifCategoryId +"'  AND ";
+        whereClause += "validity_month = '"+ year +"-"+ month +"';";
         String sql = "SELECT " + selectedFields + " FROM " + tableName + " WHERE "+whereClause +";";
-
-        int responseFromDB = executeQuery(jdbcConnectionForPrice, sql);
-
-        logger.log (Level.INFO, "DB response is: "+responseFromDB);
-
-        return responseFromDB;
+        return sql;
     }
 
     int executeQuery(String jdbcConnectionForPrice, String sqlQuery){
