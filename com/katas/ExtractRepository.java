316f600e14c6c61e5a684a159fa70c1cc92e4451
zal0001m <maxim.zalota@prosiebensat1.com>
2024-03-24 11:16:59 +0100

    Added another loadPackageDiscount() method with jdbc connection hardcoded in it

--- com/katas/ExtractRepository.java
+++ com/katas/ExtractRepository.java
@@ -4,8 +4,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-// #####Refactoring flow: "Extract Repository"
+// ##### Refactoring flow: "Extract Repository"
+// Notice 3 methods that "lookupPriceInDB()", "readDiscount()" and loadPackageDiscount() that call DB.
 //
+// --- Refactor lookupPriceInDB() method into new PriceDiscountRepositry class
 //1) Refactor: "Extract Superclass". (place cursor on class name to get that refactor menu option)
 //2) In "Extact Superclass" dialog:
 //   2a) In "Superclass name" enter PriceDiscountRepository
@@ -14,10 +16,11 @@ import java.util.logging.Logger;
 //   2d) Click "Refactor" button. Problem Detected dialog pops up with message "Field ExtractRepository.logger is private and will not be accessible from method lookupPriceInDB(int, int, int, int).".
 //   2e) Click "Continue" button in Problems Detected dialog.
 //   2f) In "Analyse and Replace Usages" dialog, click "Yes" -> there will be no additional changes made.
-//   2g) In "Add file to Git" dialog click Add. (afterward when we perform Git rollback, this file will automatically be deleted)
+//   2g) In "Add file to Git" dialog click Add. (afterward when we perform Git Rollback, this file will automatically be deleted)
 //3) In PriceDiscountRepositry class
 //   3a) Add field: private static Logger logger = Logger.getLogger(PriceDiscountRepository.class.getName());
-//   3b) Replace ExtractRepository.logger.log(...); with logger.log();
+//   3b) Replace problematic expression ExtractRepository.logger.log(...); with logger.log();
+//4) Navigate to definition of class ExtractRepository. Refactor: "Replace Inheritence with Delegation", click "Refactor" button
 
 public class ExtractRepository {
 
@@ -29,15 +32,22 @@ public class ExtractRepository {
         int baselineMonth = 01;
         int baselineYear = 2001;
 
-        return lookupPriceInDB(tarifCategory, baselineMonth, baselineYear, priceGroupId);
+        int price = lookupPriceInDB(tarifCategory, baselineMonth, baselineYear, priceGroupId);
+        if (price == 0) {
+            return loadPackagePrice(null);
+        }
+        return price;
     }
 
     private int lookupPriceInDB(int tarifCategory, int baselineMonth, int baselineYear, int priceGroupIdInt) {
-        logger.log (Level.INFO, "Reading Price from DB");
 
-        jdbcConnection = jdbcConnection+";param1=value1";
+        String jdbcConnectionForPrice = jdbcConnection+";param1=value1";
+        logger.log (Level.INFO, "Starting to read Price from DB, conn: "+jdbcConnectionForPrice);
+
         //Nonsensical logic below just simulates looking up of a value in DB. It is NOT "domain logic"
-        return priceGroupIdInt * tarifCategory + baselineYear / baselineMonth;
+        int responseFromDB = priceGroupIdInt * tarifCategory + baselineYear / baselineMonth;
+
+        return responseFromDB;
     }
 
     public double getNettoPrice(int orderId, Integer priceGroupId, int tarifCategory, String date) {
@@ -45,7 +55,7 @@ public class ExtractRepository {
         int year = Integer.parseInt(date.substring(6,10));
         try {
             int priceInEuros = lookupPriceInDB(tarifCategory, month, year, priceGroupId);
-            double discount = lookupDiscountInDB(orderId, year);
+            double discount = readDiscount(orderId, year);
             return priceInEuros*discount;
         } catch (NumberFormatException e) {
             logger.log (Level.SEVERE, "Error reading from DB");
@@ -53,11 +63,28 @@ public class ExtractRepository {
         }
     }
 
-    private double lookupDiscountInDB(int orderId, int year) {
-        logger.log (Level.INFO, "Reading Discount from DB");
-        jdbcConnection = jdbcConnection+";param2=value2";
+    private double readDiscount(int orderId, int year) {
+        String jdbcConnectionForDiscount = jdbcConnection+";param2=value2";
+        logger.log (Level.INFO, "Starting to read Discount from DB, conn: "+jdbcConnectionForDiscount);
+
         //Nonsensical logic below just simulates looking up of a value in DB. It is NOT "domain logic"
-        return Math.random()*orderId/year;
+        double responseFromDB = Math.random() * orderId / year;
+
+        return responseFromDB;
+    }
+
+    private double loadPackagePrice(Integer packageId) {
+        String jdbcConnectionForPackage = "postgresql://username:password@db.internal.com:5555/userdata?;param3=value3";
+        logger.log (Level.INFO, "Starting to read PackagePrice from DB, conn: "+jdbcConnectionForPackage);
+
+        if (packageId == null) {
+            packageId=1; //use default package
+        }
+
+        //Nonsensical logic below just simulates looking up of a value in DB. It is NOT "domain logic"
+        double responseFromDB = Math.random() + packageId;
+
+        return responseFromDB;
     }
 
 }
