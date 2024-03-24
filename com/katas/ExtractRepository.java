7a2464f807c21ca8e111249e177a2f485ca3a964
zal0001m <maxim.zalota@prosiebensat1.com>
2024-03-24 12:40:37 +0100

    Added instructions for moving loadPackagePrice() method to PriceDiscountRepository and then de-duplicating jdbcConnection string value

--- com/katas/ExtractRepository.java
+++ com/katas/ExtractRepository.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 ##### Refactoring flow: "Extract Repository"
 Notice three methods that interact DB: "lookupPriceInDB()", "readDiscount()" and loadPackageDiscount(). We will move them to a new repository class one by one.
 
+
 --- Move lookupPriceInDB() method into the new PriceDiscountRepository class
 1) Refactor: "Extract Superclass". (place cursor on class name to get that refactor menu option).
 2) In "Extract Superclass" dialog:
@@ -19,26 +20,40 @@ Notice three methods that interact DB: "lookupPriceInDB()", "readDiscount()" and
    2f) In "Analyse and Replace Usages" dialog, click "Yes".
    2g) In "Use Interface Where Possible" dialog click "OK" button.
    2h) In "Add File to Git" dialog click "Add" button. (afterward when we perform Git Rollback to reset Kata, this file will be automatically deleted)
-3) In PriceDiscountRepository class
+3) In PriceDiscountRepository class fix compilation problem due to "logger" field:
    3a) Manually add field: "private static Logger logger = Logger.getLogger(PriceDiscountRepository.class.getName());"
    3b) Replace problematic expression "ExtractRepository.logger.log(...);" with "logger.log(...);".
 4) Refactor: "Replace Inheritance with Delegation". (place cursor on ExtractRepository class name to get that refactor menu option).
-   4a) In "Delegate members" grid select lookupPriceInDB() method.
-   4b) Click "Refactor" button.
- 5) Refactor: "Inline Method" lookupPriceInDB() method in ExtractRepository class, because its a single-liner and redundant
- 6) Intention: "Change access modifier: public". For lookupPriceInDB() method in PriceDiscountRepository class
- 7) Intention: "Change access modifier: private". For jdbcConnection field in PriceDiscountRepository class
-   7a) In "Problem Detected" dialog with message "field jdbcConnection with private visibility won't be accessible from method ExtractRepository.readDiscount(int, int)" click  "Show Conflicts in View" and then jump to readDiscount() method
+5) Cleanup PriceDiscountRepository class:
+   5a) Intention: "Change access modifier: public". For lookupPriceInDB() method in PriceDiscountRepository class
+   5b) Intention: "Change access modifier: private". For jdbcConnection field in PriceDiscountRepository class
+   7b) In "Problem Detected" dialog with message "field jdbcConnection with private visibility won't be accessible from method   ExtractRepository.readDiscount(int, int)" click  "Show Conflicts in View" and then jump to readDiscount() method
 
 --- Move readDiscount() method into existing PriceDiscountRepository class
-8) Refactor: "Move Instance Method". (place cursor on readDiscount() method in ExtractRepository class.
-   8a) Click "Refactor" button.
-   8b) In "Problems Detected" dialog click "Continue" button. We ignore this message, because logger field will need to be replaced with PriceDiscountRepository's own
-9) In readDiscount() method in PriceDiscountRepository class:
-   9a) manually replace problematic expression "ExtractRepository.logger.log(...);" with "logger.log(...);"
-   9b) Refactor: "Rename": "readDiscount()" to "lookupDiscountInDB()" to be consistent with the other public method
-   9b) Intention: "Change access modifier: public". For lookupDiscountInDB() method
-10) Intention: "Change access modifier: private". For jdbcConnection field in PriceDiscountRepository class (Repetition of step 7)
+6) Refactor: "Move Instance Method". (place cursor on readDiscount() method in ExtractRepository class).
+   6a) Click "Refactor" button.
+   6b) In "Problems Detected" dialog click "Continue" button. We ignore this message, because the "logger" field will need to be replaced with PriceDiscountRepository's own.
+7) In readDiscount() method in PriceDiscountRepository class:
+   7a) Manually replace problematic expression "ExtractRepository.logger.log(...);" with "logger.log(...);"
+   7b) Intention: "Change access modifier: public". For readDiscount() method
+   7c) Refactor: "Rename": "readDiscount()" to "lookupDiscountInDB()" to be consistent with the other public method
+8) Intention: "Change access modifier: private". For jdbcConnection field in PriceDiscountRepository class (Repetition of step 7)
+
+--- Move loadPackagePrice() method into existing PriceDiscountRepository class
+9) Refactor: "Move Instance Method". (place cursor on loadPackagePrice() method in ExtractRepository class).
+   9a) Click "Refactor" button.
+   9b) In "Problems Detected" dialog click "Continue" button. We ignore this message, because the "logger" field will need to be replaced with PriceDiscountRepository's own.
+8) In loadPackagePrice() method in PriceDiscountRepository class:
+   8a) Manually replace problematic expression "ExtractRepository.logger.log(...);" with "logger.log(...);"
+   8b) Intention: "Change access modifier: public". For loadPackagePrice() method
+   8c) Refactor: "Rename": "loadPackagePrice()" to "lookupPackagePriceInDB()" to be consistent with the other public methods
+9) Try to merge jdbc connection string in lookupPackagePriceInDB with field jdbcConnection
+   9a) Refactor: Extract Field. Select seemingly duplicate string "postgresql://username:password@db.internal.com:5555/userdata?".
+   9b) In "Initialize in" select "field declaration"
+   9c) Check "Replace all occurences (2)" box. Notice that the other occurence was found and is, actually, identical!
+   9d) Name new field with some temporary name, like "jdbcConnectionTemp"
+   9e) Refactor: "Inline Field", the origingal jdbcConnection, Select "Inline all and remove the field"
+   9f) Refactor: Rename "jdbcConnectionTemp" to "jdbcConnection"
 
 */
 
