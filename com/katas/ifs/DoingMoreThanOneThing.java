647ed8a8f54729714bec32c5dd212c75c1f0b81f
zal0001m <maxim.zalota@prosiebensat1.com>
2024-03-29 11:49:59 +0100

    A bit of refactoring

--- com/katas/ifs/DoingMoreThanOneThing.java
+++ com/katas/ifs/DoingMoreThanOneThing.java
@@ -2,43 +2,46 @@ package com.katas.ifs;
 
 public class DoingMoreThanOneThing {
 
-    public void doingTwoThingsSmaller(boolean isHungry, int defaultValue) {
+    public double doingTwoThingsSmaller(boolean isHungry, int defaultValue) {
+        double minimumWagePerHour = 18.5;
 
         //culculate the Number and the Factor
-        int barNumber = 0;
-        double fooFactor = 1;
+        int ingredientsCost = 0;
+        double cookingTimeHours = 1;
         if (isHungry) {
             System.out.println("In if top: she is hungry");
-            barNumber = barMethodAAA("ACTION - eat the food AA");
-            fooFactor = fooMethodAAA("ACTION - cook something tasty AA");
+            ingredientsCost = calculateLargeNumber("ACTION - eat the food AA");
+            cookingTimeHours = calculateLargeFactor("ACTION - cook something tasty AA");
         } else {
-            barNumber = barMethod("ACTION - eat the food CCC");
-            fooFactor = fooMethod("ACTION - cook something tasty CCC");
+            ingredientsCost = calculateSmallNumber("ACTION - eat the food CCC");
+            cookingTimeHours = calculateSmallFactor("ACTION - cook something tasty CCC");
         }
 
         //Save to DB if necessary
-        if (fooFactor==0){
+        if (cookingTimeHours==0){
             saveToDB(String.valueOf(defaultValue));
             System.out.println("Saved default value to DB");
         } else {
-            double result = fooFactor * barNumber;
-            System.out.println("Saving calculated result to DB");
-            saveToDB(String.valueOf(result));
+            double mealCost = cookingTimeHours*minimumWagePerHour + ingredientsCost;
+            System.out.println("Saving calculated mealCost to DB");
+            saveToDB(String.valueOf(mealCost));
         }
+
+        return cookingTimeHours;
     }
-    protected double fooMethodAAA(String message) {
+    protected double calculateLargeFactor(String message) {
         return 5.4;
     }
 
-    protected double fooMethod(String message) {
+    protected double calculateSmallFactor(String message) {
         return 5.4;
     }
 
-    protected int barMethod(String message) {
+    protected int calculateSmallNumber(String message) {
         System.out.println(message);
         return 7;
     }
-    protected int barMethodAAA(String message) {
+    protected int calculateLargeNumber(String message) {
         System.out.println(message);
         return 7;
     }
