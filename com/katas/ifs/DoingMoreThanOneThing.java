f426b0d50833d132691164e65ea81c250c13a6b8
zal0001m <maxim.zalota@prosiebensat1.com>
2024-03-29 08:15:20 +0100

    I think I got doingTwoThingsSmaller set up for refactoring

---
+++ com/katas/ifs/DoingMoreThanOneThing.java
@@ -0,0 +1,64 @@
+package com.katas.ifs;
+
+public class DoingMoreThanOneThing {
+
+
+
+    public void doingTwoThings(boolean isHungry, boolean foodIsReady) {
+        int barNumber = 0;
+        double fooFactor = 1;
+        if (isHungry) {
+            System.out.println("In if top: she is hungry");
+            barNumber = barMethod("ACTION - eat the food AA");
+            fooFactor = fooMethod("ACTION - cook something tasty AA");
+            if (foodIsReady) {
+                fooFactor = fooMethod("ACTION - cook something tasty BB ");
+            } else {
+                barNumber = barMethod("ACTION - eat the food BBB");
+            }
+
+        } else {
+            if (foodIsReady) {
+                barNumber = barMethod("ACTION - eat the food CCC");
+            } else {
+                fooFactor = fooMethod("ACTION - cook something tasty CCC");
+            }
+            System.out.println("In else: she is NOT hungry");
+        }
+
+        double result = fooFactor * barNumber;
+        System.out.println("Saving result to DB: "+result);
+    }
+
+    public void doingTwoThingsSmaller(boolean isHungry, boolean foodIsReady) {
+        int barNumber = 0;
+        double fooFactor = 1;
+        if (isHungry) {
+            System.out.println("In if top: she is hungry");
+            barNumber = barMethodAAA("ACTION - eat the food AA");
+            fooFactor = fooMethodAAA("ACTION - cook something tasty AA");
+        } else {
+            barNumber = barMethod("ACTION - eat the food CCC");
+            fooFactor = fooMethod("ACTION - cook something tasty CCC");
+        }
+
+        double result = fooFactor * barNumber;
+        System.out.println("Saving result to DB: "+result);
+    }
+    protected double fooMethodAAA(String message) {
+        return 5.4;
+    }
+
+    protected double fooMethod(String message) {
+        return 5.4;
+    }
+
+    protected int barMethod(String message) {
+        System.out.println(message);
+        return 7;
+    }
+    protected int barMethodAAA(String message) {
+        System.out.println(message);
+        return 7;
+    }
+}
