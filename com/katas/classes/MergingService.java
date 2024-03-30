d59042976a9b73206e48ec6c928b1c963076841d
zal0001m <maxim.zalota@prosiebensat1.com>
2024-03-30 12:55:15 +0100

    Created code of "merging two classes" Refactoring Flow

--- com/katas/classes/MergingService.java
+++ com/katas/classes/MergingService.java
@@ -5,18 +5,16 @@ import java.util.logging.Logger;
 
 
 public class MergingService {
-
     private static Logger logger = Logger.getLogger(MergingService.class.getName());
+
+    private final MergeOneRepository mergeOneRepository = new MergeOneRepository();
     private final MergeTwoRepository mergeTwoRepository = new MergeTwoRepository();
 
-
     public double getDiscount( String date, int orderId) {
-
-        int month = Integer.parseInt(date.substring(3,5));
         int year = Integer.parseInt(date.substring(6,10));
         double discount;
         try {
-            discount = new MergeOneRepository().loadDiscountFromDB(orderId, year);
+            discount = mergeOneRepository.loadDiscountFromDB(orderId, year);
         } catch (NumberFormatException e) {
             logger.log (Level.SEVERE, "Error reading from DB");
             return 0;
