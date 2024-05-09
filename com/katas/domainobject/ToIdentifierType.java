2065c47662281a87bd548b94a99732d6c7ad1796
zal0001m <maxim.zalota@prosiebensat1.com>
2024-05-09 12:43:31 +0200

    Cleaned up simplified ToIdentifierType kata

--- com/katas/domainobject/ToIdentifierType.java
+++ com/katas/domainobject/ToIdentifierType.java
@@ -6,13 +6,13 @@ public class ToIdentifierType {
      *
      * 1) Refactor: Introduce Parameter Object. Cursor on "lookupPriceInDB()" method. In "Introduce Parameter Object" dialog:
      *    1a) In "Create new class Name" box enter "PriceGroupId".
-     *    1b) In "Parameters to Extract" grid select "int priceGroupID" only.
+     *    1b) In "Parameters to Extract" grid select only "int priceGroupID" parameter.
      * 	  1c) Click "Refactor" button.
      *    1d) In "Add File to Git" dialog click "Add" button.
      *  2) Refactor: Introduce Parameter Object. Cursor on "lookupDiscountInDB()" method. In "Introduce Parameter Object" dialog:
      *    2a) Select "Use exiting class" radio button.
      *    2b) In "Name" box enter "com.katas.PriceGroupId".
-     *    2c) In "Parameters to Extract" grid select "int priceGroupId" only.
+     *    2c) In "Parameters to Extract" grid select only "int priceGroupId" parameter.
      *    2d) Click "Refactor" button.
      * 3) Refactor: Introduce Parameter. Place cursor in "calculateNettoPrice()" method on "new PriceGroupId(priceGroupId)". Replace all 2 occurrences.
      * 4) Refactor: Introduce Variable. Place cursor in "controllerGetPrice()" method on "new PriceGroupId(priceGroupId)".
