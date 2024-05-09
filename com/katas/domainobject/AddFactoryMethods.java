a58012dc5abfb857b97c8cafab035a60aa3f01ee
zal0001m <maxim.zalota@prosiebensat1.com>
2024-05-09 13:23:34 +0200

    cleaning up AddFactoryMethods kata

--- com/katas/domainobject/AddFactoryMethods.java
+++ com/katas/domainobject/AddFactoryMethods.java
@@ -14,25 +14,25 @@ public class AddFactoryMethods {
      *    1b) In "Parameters to Extract" grid select "int month" and "int year" only.
      * 	  1c) Click "Refactor" button.
      *    1d) In "Add File to Git" dialog click "Add" button.
-     * 3) Refactor: Introduce Variable. Place cursor in "calculateNettoPrice()" method on "new MonthYear(month, year)".
-     * 4) Refactor: Inline Variable. Place cursor in "calculateNettoPrice()" method on "month" variable.
-     * 5) Refactor: Inline Variable. Place cursor in "calculateNettoPrice()" method on "year" variable.
-     * 6) Refactor: Extract Method. Select in "controllerGetPrice()" snippet: "new MonthYear(...)".
-     * 7) Refactor: Move Members. Place cursor on newly created "getMonthYear()" method definition. In "Move Static Members" dialog:
-     *    7a) In "To (fully qualified name)" box enter "com.katas.domainobject.MonthYear".
-     *    7b) In "Members to be moved (static only)" grid, select "getMonthYear()" method.
-     *    7c) Click "Refactor" button
-     * 8) Intention: Replace constructor with factory method. Place cursor on "MonthYear(int, int)" constructor. In "Replace Constructor With Factory Method" dialog:
-     *    8a) In "Factory method name" box enter "createFromIntegers".
-     * 	  8b) Click "Refactor" button.
-     * 9) Refactor: Rename. Static factory method "getMonthYear(String)" to be "createFromString()".
-     * 10) Refactor: Introduce Parameter. Place cursor in "calculateNettoPrice()" method on "monthYear" variable.
-     * 11) Refactor: Introduce Variable. Place cursor in "controllerGetPrice()" method on "MonthYear.createFromString(today)" snippet.
-     * 12) Refactor: Extract Method. Select first 3 code lines in "controllerGetPrice()" method, name it "createFromToday()"
-     * 13) Refactor: Move Members. Place cursor on newly created "createFromToday()" method definition. In "Move Static Members" dialog:
-     *    13a) In "To (fully qualified name)" box enter "com.katas.domainobject.MonthYear".
-     *    13b) In "Members to be moved (static only)" grid, select "createFromToday()" method.
-     *    13c) Click "Refactor" button
+     * 2) Refactor: Introduce Variable. Place cursor in "calculateNettoPrice()" method on "new MonthYear(month, year)".
+     * 3) Refactor: Inline Variable. Place cursor in "calculateNettoPrice()" method on "month" variable.
+     * 4) Refactor: Inline Variable. Place cursor in "calculateNettoPrice()" method on "year" variable.
+     * 5) Refactor: Extract Method. Select in "controllerGetPrice()" snippet: "new MonthYear(...)".
+     * 6) Refactor: Move Members. Place cursor on newly created "getMonthYear()" method definition. In "Move Static Members" dialog:
+     *    6a) In "To (fully qualified name)" box enter "com.katas.domainobject.MonthYear".
+     *    6b) In "Members to be moved (static only)" grid, select "getMonthYear()" method.
+     *    6c) Click "Refactor" button
+     * 7) Intention: Replace constructor with factory method. Place cursor on "MonthYear(int, int)" constructor. In "Replace Constructor With Factory Method" dialog:
+     *    7a) In "Factory method name" box enter "createFromIntegers".
+     * 	  7b) Click "Refactor" button.
+     * 8) Refactor: Rename. Static factory method "getMonthYear(String)" to be "createFromString()".
+     * 9) Refactor: Introduce Parameter. Place cursor in "calculateNettoPrice()" method on "monthYear" variable.
+     * 10) Refactor: Introduce Variable. Place cursor in "controllerGetPrice()" method on "MonthYear.createFromString(today)" snippet.
+     * 11) Refactor: Extract Method. Select first 3 code lines in "controllerGetPrice()" method, name it "createFromToday()"
+     * 12) Refactor: Move Members. Place cursor on newly created "createFromToday()" method definition. In "Move Static Members" dialog:
+     *    12a) In "To (fully qualified name)" box enter "com.katas.domainobject.MonthYear".
+     *    12b) In "Members to be moved (static only)" grid, select "createFromToday()" method.
+     *    12c) Click "Refactor" button
      *
      * Now MonthYear class has three clean factory methods:
      * - createFromString(String)
