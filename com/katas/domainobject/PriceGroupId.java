3a7727f2dd4084430bd451b51ae20bdbceafb573
zal0001m <maxim.zalota@prosiebensat1.com>
2024-05-09 12:17:33 +0200

    Created simplified ToIdentifierType kata

---
+++ com/katas/domainobject/PriceGroupId.java
@@ -0,0 +1,13 @@
+package com.katas.domainobject;
+
+public class PriceGroupId {
+    private final int priceGroupID;
+
+    public PriceGroupId(int priceGroupID) {
+        this.priceGroupID = priceGroupID;
+    }
+
+    public int getPriceGroupID() {
+        return priceGroupID;
+    }
+}
---
+++ com/katas/domainobject/TarifCategoryId.java
@@ -0,0 +1,13 @@
+package com.katas.domainobject;
+
+public class TarifCategoryId {
+    private final int tarifCategory;
+
+    public TarifCategoryId(int tarifCategory) {
+        this.tarifCategory = tarifCategory;
+    }
+
+    public int getTarifCategory() {
+        return tarifCategory;
+    }
+}
