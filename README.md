341b9c42dc60d01f9e1b16a2918bec21d83ec617
zal0001m <maxim.zalota@prosiebensat1.com>
2025-03-10 11:04:21 +0100

    Improved instructions in ExtrctIdentiferType class to fix a problem. Added a link to a new refactoring videos

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 
 ## Intentions used in Kata
 
-1a."Extract if (condition)"
-1b. "Merge nested 'ifs'"
-2"Invert if condition"
+1a) "Extract if (condition)"
+1b) "Merge nested 'ifs'"
+2) "Invert if condition"
 
-3a. "Move up into if statement branches"
-3b. "Extract common part from 'if'"
-3bb "Extract common part from 'if'(may change semantics)"
+3a) "Move up into if statement branches"
+3b) "Extract common part from 'if'"
+3bb) "Extract common part from 'if'(may change semantics)"
 
-4a. "Split 'else if'"
-4b. Merge 'else if'
+4a) "Split 'else if'"
+4b) Merge 'else if'
 
-5. "Remove redundant else"
+5)"Remove redundant else"
 
-6. "Transform body to single exit-point form"
+6)"Transform body to single exit-point form"
 
 
 "Reuse previous variable 'message' declaration"
@@ -38,6 +38,12 @@ Three-step refactoring, "extract-inline-rename" with introducing a temporary nam
 1) Upstack, Donwstack 
 2) De-Duplicate
 
-References to additional Refactorings:
+References to additional refactoring videos
+By Dmitry Kandalov - IDEAs
+- Gilded Rose Refactoring Kata https://www.youtube.com/watch?v=AxxNHKCldzA
+
+By Emily Bache (https://www.youtube.com/@EmilyBache-tech-coach):
 - Split Variable: https://www.youtube.com/watch?v=wPmJz2ynb3k
 - Split Loop: https://www.youtube.com/watch?v=yYC7eaRVc7M
+- Conditionals/Ifs (Guard clause): https://www.youtube.com/watch?v=fLaXlBVUb0c
+
--- com/katas/domainobject/ExtractIdentifierType.java
+++ com/katas/domainobject/ExtractIdentifierType.java
@@ -1,30 +1,36 @@
 package com.katas.domainobject;
 
 public class ExtractIdentifierType {
-    /*
+
     /* ## Replace primitive with "Identifier Type". (https://medium.com/@gara.mohamed/domain-driven-design-the-identifier-type-pattern-d86fd3c128b3)
      *
-     * 1) Refactor: Introduce Parameter Object. Cursor on "lookupPriceInDB()" method. In "Introduce Parameter Object" dialog:
+     * 1) Refactor: Introduce Parameter Object. Cursor on the "lookupPriceInDB()" method. In "Introduce Parameter Object" dialog:
      *    1a) In "Create new class Name" box enter "PriceGroupId".
      *    1b) In "Parameters to Extract" grid select only "int priceGroupID" parameter.
      * 	  1c) Click "Refactor" button.
      *    1d) In "Add File to Git" dialog click "Add" button.
-     * 2) Refactor: Introduce Parameter Object. Cursor on "lookupDiscountInDB()" method. In "Introduce Parameter Object" dialog:
-     *    2a) Select "Use exiting class" radio button.
-     *    2b) In "Name" box enter "com.katas.domainobject.PriceGroupId".
-     *    2c) In "Parameters to Extract" grid select only "int priceGroupId" parameter.
-     *    2d) Click "Refactor" button.
-     * 3) Refactor: Introduce Parameter. Place cursor in "calculateNettoPrice()" method on "new PriceGroupId(priceGroupId)". Replace all 2 occurrences.
-     * 4) Refactor: Introduce Variable. Place cursor in "controllerGetPrice()" method on "new PriceGroupId(priceGroupId)".
-     * 5) Refactor: Inline Variable. Place current in "controllerGetPrice()" method on "priceGroupId" variable.
-     * 6) Refactor: Rename. Method "getPriceGroupID()" in class "PriceGroupId" to be "asInt()".
-     * 7) Generate @equals() and @hashCode() for PriceGroupId class so that two separate instances of PriceGroupId that hold the same integer value would be equal. Set cursor on PriceGroupId class definition and use shortcut "alt+insert".
+     * 2) Intention: Convert record to class. Cursor on the new "PriceGroupId" class.
+     * 3) Refactor: Introduce Parameter Object. Cursor on "lookupDiscountInDB()" method. In "Introduce Parameter Object" dialog:
+     *    3a) Select "Use exiting class" radio button.
+     *    3b) In "Name" box enter "com.katas.domainobject.PriceGroupId".
+     *    3c) In "Parameters to Extract" grid select only "int priceGroupId" parameter.
+     *    3d) Click "Refactor" button.
+     * 4) Refactor: Introduce Parameter. Place the cursor in "calculateNettoPrice()" method on "new PriceGroupId(priceGroupId)". Replace all 2 occurrences.
+     * 5) Refactor: Introduce Variable. Place the cursor in "controllerGetPrice()" method on "new PriceGroupId(priceGroupId)".
+     * 6) Refactor: Inline Variable. Place the cursor in "controllerGetPrice()" method on "priceGroupId" variable.
      +
+     * Clean up PriceGroupId class; it should have only one getter method - "asInt()".
+     * 7) Refactor: Extract Method. Place the cursor in "getPriceGroupID()" on priceGroupId field. Give method name "asInt()". Replace all occurrences.
+     * 8) Intention: "Change access modifier: public". For asInt() method
+     * 9) Refactor: Inline Method. Place the cursor on "getPriceGroupID()" method of class "PriceGroupId".
+     * 10) Refactor: Inline Method. Place the cursor on "priceGroupID()" method of class "PriceGroupId".
+     *
      *
      * Repeat this refactoring flow to create TarifCategoryId class from "int tarifCategory" primitive.
      *
      * Bonus: Create a new domain object PriceSpecification, which is a combination of TarifCategoryId and PriceGroupId
      */
+
     public double controllerGetPrice(final int tarifCategoryInt, final int priceGroupIdInt) {
         return this.calculateNettoPrice(tarifCategoryInt, priceGroupIdInt);
     }
