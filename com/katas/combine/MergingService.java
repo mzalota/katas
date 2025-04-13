29d2831ca37f0261fe954a799e9738cfc0c2770b
zal0001m <maxim.zalota@prosiebensat1.com>
2025-04-13 12:12:17 +0200

    Cleaned up instructions in ExtractRepository and MergeRepositories

--- com/katas/combine/MergingService.java
+++ com/katas/combine/MergingService.java
@@ -5,41 +5,42 @@ import java.util.logging.Logger;
 
 
 /*
- * Combine MergeOneRepository and MergeTwoRepository classes into CommonRepository class, without breaking any existing references.
- *
- * -- Deal with the MergeOneRepository class
- * 1) Refactor: Extract Superclass. Place cursor on "loadFromDB()" method in "MergeOneReposory" class:
- *    1a) In "Superclass name" box enter "CommonRepository".
- *    1b) In "Members to Form Superclass" grid select all 3 members and then click "Refactor" button.
- *    1c) In "Analyze and Replace Usages" dialog click "Yes".
- *    1d) In "Rename Variables" dialog click "Select all" button and then click "OK".
- *    1e) In "Add File to Git" dialog click "Add" button.
- * 2) In CommonRepository class, in definition of logger field, replace "MergeOneRepository" class with "CommonRepository".
- * 3) Refactor: Inline. Place cursor on "MergeOneReposory" class. Click "Refactor" button.
- *
- * -- Deal with the MergeTwoRepository class
- * 4) Manually add "extends CommonRepository" to MergeTwoRepository class definition.
- * 5) Refactor: Pull Members Up Superclass. Place cursor on "loadFromDB()" method in "MergeTwoReposory" class:
- *    5a) In "Members to be pulled up", select all members.
- *    5b) In Problems Detected we see messages that Common Repository already contains loadFromDB() method and logger field. Click "Cancel"
- *    5c) In "Pull Members Up" dialog click "Cancel" button.
- * 6) Refactor: Rename... "loadFromDB()" in MergeTwoReposory class to "loadPriceFromDB()". Select "Refactor only current method".
- * 7) Refactor: Pull Members Up Superclass. Place cursor on "loadPriceFromDB()" method in "MergeTwoReposory" class
- *    7a) In "Members to be pulled up" check "loadPriceFromDB()" and "jdbcConnection:String". Don't select "logger" field
- * 	  7b) In Problems Detected we see message that "logger" will not be accessible. We ignore this problem. Click "Continue" button.
- * 8) In "CommonRepository.loadPriceFromDB()" method replace problematic "MergeTwoRepository.logger" with "logger"
- * 9) In "MergeTwoRepository" class delete "logger" field, so that the class is empty.
- * 10) Refactor: Inline. Place cursor on "MergeTwoRepository" class. Click "Refactor" button.
- *
- * -- Clean up the MergingService class
- * 11) Refactor: Extract Field. In "MergingService", select "new CommonRepository()" code snippet. Check "Replace all occurrences(2)" box. Field "commonRepository" will be created.
- * 12) Refactor: Inline Field. "mergeOneRepository"
- * 13) Refactor: Inline Field. "mergeTwoRepository"
- *
- * -- Clean up the CommonRepository class
- * 14) In CommonRepository class deduplicate "jdbcConnection*" fields and "load*FromDB" methods (see DeDuplicateFunctions kata)
- *
- */
+##### Refactoring flow: "Merge two Repositories into One"
+Combine MergeOneRepository and MergeTwoRepository classes into CommonRepository class, without breaking any existing references.
+
+--- Deal with the MergeOneRepository class
+ 1) Refactor: Extract Superclass. Place cursor on "loadFromDB()" method in "MergeOneRepository" class:
+    1a) In "Superclass name" box enter "CommonRepository".
+    1b) In "Members to Form Superclass" grid select all 3 members and then click "Refactor" button.
+    1c) In "Analyze and Replace Usages" dialog click "Yes".
+    1d) In "Rename Variables" dialog click "Select all" button and then click "OK".
+    1e) In "Add File to Git" dialog click "Add" button.
+ 2) In CommonRepository class, in definition of logger field, replace "MergeOneRepository" class with "CommonRepository".
+ 3) Refactor: Inline. Place cursor on "MergeOneReposory" class. Click "Refactor" button.
+
+--- Deal with the MergeTwoRepository class
+ 4) Manually add "extends CommonRepository" to MergeTwoRepository class definition.
+ 5) Refactor: Pull Members Up Superclass. Place cursor on "loadFromDB()" method in "MergeTwoReposory" class:
+    5a) In "Members to be pulled up", select all members.
+    5b) In Problems Detected we see messages that Common Repository already contains loadFromDB() method and logger field. Click "Cancel"
+    5c) In "Pull Members Up" dialog click "Cancel" button.
+ 6) Refactor: Rename... "loadFromDB()" in MergeTwoReposory class to "loadPriceFromDB()". Select "Refactor only current method".
+ 7) Refactor: Pull Members Up Superclass. Place cursor on "loadPriceFromDB()" method in "MergeTwoReposory" class
+    7a) In "Members to be pulled up" check "loadPriceFromDB()" and "jdbcConnection:String". Don't select "logger" field
+    7b) In Problems Detected we see message that "logger" will not be accessible. We ignore this problem. Click "Continue" button.
+ 8) In "CommonRepository.loadPriceFromDB()" method replace problematic "MergeTwoRepository.logger" with "logger"
+ 9) In "MergeTwoRepository" class delete "logger" field, so that the class is empty.
+ 10) Refactor: Inline. Place cursor on "MergeTwoRepository" class. Click "Refactor" button.
+
+--- Clean up the MergingService class
+ 11) Refactor: Extract Field. In "MergingService", select "new CommonRepository()" code snippet. Check "Replace all occurrences(2)" box. Field "commonRepository" will be created.
+ 12) Refactor: Inline Field. "mergeOneRepository"
+ 13) Refactor: Inline Field. "mergeTwoRepository"
+
+--- Clean up the CommonRepository class
+ 14) In CommonRepository class deduplicate "jdbcConnection*" fields and "load*FromDB" methods (see DeDuplicateFunctions kata)
+
+*/
 
 
 public class MergingService {
--- com/katas/dependency/ExtractRepository.java
+++ com/katas/dependency/ExtractRepository.java
@@ -10,50 +10,49 @@ Notice three methods that interact DB: "lookupPriceInDB()", "readDiscount()" and
 
 
 --- Move lookupPriceInDB() method into the new PriceDiscountRepository class
-1) Refactor: "Extract Superclass". (place cursor on class name to get that refactor menu option).
-2) In "Extract Superclass" dialog:
-   2a) In "Superclass name" enter PriceDiscountRepository
-   2b) In "Members To Form Superclass" grid select "lookupPriceInDB()". You see now that "jdbcConnection:String" turned color red, because it is "used by lookupPriceInDB(..)" (hover mouse to see explanation message) and must also be extracted.
-   2c) In "Members To Form Superclass" grid select "jdbcConnection:String". Nothing else turned red. You see "logger:Logger" is color blue, because it is "used by lookupPriceInDB(..)" (hover mouse to see explanation message). We will ignore this field, because new class PriceDiscountRepository will need to have its own logger field
-   2d) Click "Refactor" button.
-   2e) In "Problems Detected" click "Continue" button. We ignore message "Field ExtractRepository.logger is private and will not be accessible from method lookupPriceInDB(int, int, int, int).", because the new class will need its own logger field.
-   2f) In "Analyse and Replace Usages" dialog, click "Yes".
-   2g) In "Use Interface Where Possible" dialog click "OK" button.
-   2h) In "Add File to Git" dialog click "Add" button. (afterward when we perform Git Rollback to reset Kata, this file will be automatically deleted)
-3) In PriceDiscountRepository class fix compilation problem due to "logger" field:
-   3a) Manually add field: "private static Logger logger = Logger.getLogger(PriceDiscountRepository.class.getName());"
-   3b) Replace problematic expression "ExtractRepository.logger.log(...);" with "logger.log(...);".
-4) Refactor: "Replace Inheritance with Delegation". (place cursor on ExtractRepository class name to get that refactor menu option).
-5) Cleanup PriceDiscountRepository class:
-   5a) Intention: "Change access modifier: public". For lookupPriceInDB() method in PriceDiscountRepository class
-   5b) Intention: "Change access modifier: private". For jdbcConnection field in PriceDiscountRepository class
-   7b) In "Problem Detected" dialog with message "field jdbcConnection with private visibility won't be accessible from method   ExtractRepository.readDiscount(int, int)" click  "Show Conflicts in View" and then jump to readDiscount() method
+ 1) Refactor: "Extract Superclass". Place cursor on class name ExtractRepository. In "Extract Superclass" dialog:
+    1a) In "Superclass name" box enter PriceDiscountRepository
+    1b) In "Members To Form Superclass" grid select "lookupPriceInDB()". You see now that "jdbcConnection:String" turned color red, because it is "used by lookupPriceInDB(..)" (hover mouse to see explanation message) and must also be extracted.
+    1c) In "Members To Form Superclass" grid select "jdbcConnection:String". Nothing else turned red. You see "logger:Logger" is color blue, because it is "used by lookupPriceInDB(..)" (hover mouse to see explanation message). We will ignore this field, because new class PriceDiscountRepository will need to have its own logger field
+    1d) Click "Refactor" button.
+    1e) In "Problems Detected" click "Continue" button. We ignore message "Field ExtractRepository.logger is private and will not be accessible from method lookupPriceInDB(int, int, int, int).", because the new class will need its own logger field.
+    1f) In "Analyse and Replace Usages" dialog, click "Yes".
+    1g) In "Use Interface Where Possible" dialog click "OK" button.
+    1h) In "Add File to Git" dialog click "Add" button. (afterward when we perform Git Rollback to reset Kata, this file will be automatically deleted)
+ 2) In PriceDiscountRepository class fix compilation problem due to "logger" field:
+    2a) Manually add field: "private static Logger logger = Logger.getLogger(PriceDiscountRepository.class.getName());"
+    2b) Replace problematic expression "ExtractRepository.logger.log(...);" with "logger.log(...);".
+ 3) Refactor: "Replace Inheritance with Delegation". (place cursor on ExtractRepository class name to get that refactor menu option).
+ 4) Cleanup PriceDiscountRepository class:
+    4a) Intention: "Change access modifier: public". For lookupPriceInDB() method in PriceDiscountRepository class
+    4b) Intention: "Change access modifier: private". For jdbcConnection field in PriceDiscountRepository class
+    4c) In "Problem Detected" dialog with message "field jdbcConnection with private visibility won't be accessible from method   ExtractRepository.readDiscount(int, int)" click  "Show Conflicts in View" and then jump to readDiscount() method
 
 --- Move readDiscount() method into existing PriceDiscountRepository class
-6) Refactor: "Move Instance Method". (place cursor on readDiscount() method in ExtractRepository class).
-   6a) Click "Refactor" button.
-   6b) In "Problems Detected" dialog click "Continue" button. We ignore this message, because the "logger" field will need to be replaced with PriceDiscountRepository's own.
-7) In readDiscount() method in PriceDiscountRepository class:
-   7a) Manually replace problematic expression "ExtractRepository.logger.log(...);" with "logger.log(...);"
-   7b) Intention: "Change access modifier: public". For readDiscount() method
-   7c) Refactor: "Rename": "readDiscount()" to "lookupDiscountInDB()" to be consistent with the other public method
-8) Intention: "Change access modifier: private". For jdbcConnection field in PriceDiscountRepository class (Repetition of step 7)
+ 5) Refactor: "Move Instance Method". (place cursor on readDiscount() method in ExtractRepository class).
+    5a) Click "Refactor" button.
+    5b) In "Problems Detected" dialog click "Continue" button. We ignore this message, because the "logger" field will need to be replaced with PriceDiscountRepository's own.
+ 6) In readDiscount() method in PriceDiscountRepository class:
+    6a) Manually replace problematic expression "ExtractRepository.logger.log(...);" with "logger.log(...);"
+    6b) Intention: "Change access modifier: public". For readDiscount() method
+    6c) Refactor: "Rename": "readDiscount()" to "lookupDiscountInDB()" to be consistent with the other public method
+ 7) Intention: "Change access modifier: private". For jdbcConnection field in PriceDiscountRepository class (Repetition of step 7)
 
 --- Move loadPackagePrice() method into existing PriceDiscountRepository class
-9) Refactor: "Move Instance Method". (place cursor on loadPackagePrice() method in ExtractRepository class).
-   9a) Click "Refactor" button.
-   9b) In "Problems Detected" dialog click "Continue" button. We ignore this message, because the "logger" field will need to be replaced with PriceDiscountRepository's own.
-8) In loadPackagePrice() method in PriceDiscountRepository class:
-   8a) Manually replace problematic expression "ExtractRepository.logger.log(...);" with "logger.log(...);"
-   8b) Intention: "Change access modifier: public". For loadPackagePrice() method
-   8c) Refactor: "Rename": "loadPackagePrice()" to "lookupPackagePriceInDB()" to be consistent with the other public methods
-9) Try to merge jdbc connection string in lookupPackagePriceInDB with field jdbcConnection
-   9a) Refactor: Extract Field. Select seemingly duplicate string "postgresql://username:password@db.internal.com:5555/userdata?".
-   9b) In "Initialize in" select "field declaration"
-   9c) Check "Replace all occurrences (2)" box. Notice that the other occurrence was found and is, actually, identical!
-   9d) Name new field with some temporary name, like "jdbcConnectionTemp"
-   9e) Refactor: "Inline Field", the original jdbcConnection, Select "Inline all and remove the field"
-   9f) Refactor: Rename "jdbcConnectionTemp" to "jdbcConnection"
+ 8) Refactor: "Move Instance Method". (place cursor on loadPackagePrice() method in ExtractRepository class).
+    8a) Click "Refactor" button.
+    8b) In "Problems Detected" dialog click "Continue" button. We ignore this message, because the "logger" field will need to be replaced with PriceDiscountRepository's own.
+ 9) In loadPackagePrice() method in PriceDiscountRepository class:
+    9a) Manually replace problematic expression "ExtractRepository.logger.log(...);" with "logger.log(...);"
+    9b) Intention: "Change access modifier: public". For loadPackagePrice() method
+    9c) Refactor: "Rename": "loadPackagePrice()" to "lookupPackagePriceInDB()" to be consistent with the other public methods
+ 10) Try to merge jdbc connection string in lookupPackagePriceInDB with field jdbcConnection
+    10a) Refactor: Extract Field. Select seemingly duplicate string "postgresql://username:password@db.internal.com:5555/userdata?".
+    10b) In "Initialize in" select "field declaration"
+    10c) Check "Replace all occurrences (2)" box. Notice that the other occurrence was found and is, actually, identical!
+    10d) Name new field with some temporary name, like "jdbcConnectionTemp"
+    10e) Refactor: "Inline Field", the original jdbcConnection, Select "Inline all and remove the field"
+    10f) Refactor: Rename "jdbcConnectionTemp" to "jdbcConnection"
 
 */
 
