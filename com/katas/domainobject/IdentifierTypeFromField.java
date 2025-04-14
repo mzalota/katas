7912470362af7bc0a6ca579bfb34f26b4404000a
zal0001m <maxim.zalota@prosiebensat1.com>
2025-04-14 16:07:44 +0200

    IdentifierTypeFromField Looks good now, I think

--- com/katas/domainobject/IdentifierTypeFromField.java
+++ com/katas/domainobject/IdentifierTypeFromField.java
@@ -5,31 +5,32 @@ public class IdentifierTypeFromField {
     /* ## Refactoring flow: Replace primitive-typed field with "Identifier Type".
      * (Ref: https://medium.com/@gara.mohamed/domain-driven-design-the-identifier-type-pattern-d86fd3c128b3)
      *
-     * 1) Refactor: Encapsulate Fields. Cursor on the declaration of "customerId" field. In "Encapsulate Fields" dialog:
-     *    1a) Select only "customerId" field. Don't select "name" field
-     *    1b) Select all boxes in Encapsulate section: "Get access", "Set access", "Use accessors even when field is accessible"
-     * 	  1c) Click "Refactor" button.
-     * 2) Refactor: Introduce Parameter Object. Cursor on the "setCustomerId()" method. In "Introduce Parameter Object" dialog:
-     *    2a) In "Create new class Name" box enter "CustomerId".
-     *    2b) In "Parameters to Extract" grid select "double customerId" parameter.
+     * 1) Manually remove "final" keyword in the declaration of "customerId" field. Otherwise we cannot define a setter method for it.
+     * 2) Refactor: Encapsulate Fields. Cursor on the declaration of "customerId" field. In "Encapsulate Fields" dialog:
+     *    2a) Select only "customerId" field. Don't select "name" field
+     *    2b) Select all boxes in Encapsulate section: "Get access", "Set access", "Use accessors even when field is accessible"
      * 	  2c) Click "Refactor" button.
-     *    2d) In "Add File to Git" dialog click "Add" button.
-     * 3) Intention: Convert record to class. Cursor on the new "CustomerId" class.
-     * 4) Refactor: Extract Field. Place the cursor on newly created customerId1 parameter in setCustomerId() method.
-     * 5) In getCustomerId() method manually replace "return customerId;" with "return customerId1.getCustomerId();"
-     * 6) In setCustomerId() method manually delete line "this.customerId = this.customerId1.getCustomerId();"
-     * 7) Intention: "remove field customerId". Place the cursor on the definition of the customerId field.
+     * 3) Refactor: Introduce Parameter Object. Cursor on the "setCustomerId()" method. In "Introduce Parameter Object" dialog:
+     *    3a) In "Create new class Name" box enter "CustomerId".
+     *    3b) In "Parameters to Extract" grid select "double customerId" parameter.
+     * 	  3c) Click "Refactor" button.
+     *    3d) In "Add File to Git" dialog click "Add" button.
+     * 4) Intention: Convert record to class. Cursor on the new "CustomerId" class.
+     * 5) Refactor: Extract Field. Place the cursor on newly created customerId1 parameter in setCustomerId() method.
+     * 6) In getCustomerId() method manually replace "return customerId;" with "return customerId1.getCustomerId();"
+     * 7) In setCustomerId() method manually delete line "this.customerId = this.customerId1.getCustomerId();"
+     * 8) Intention: "remove field customerId". Place the cursor on the definition of the customerId field.
      *
      * --- Clean up
-     * 7) Refactor: Rename. customerId1 -> customerId.
-     * 8) Refactor: Inline Method setCustomerId(). Choose option to remove setCustomerId() method - public setters are not welcomed.
-     * 9) Refactor: Rename getCustomerId() to getCustomerIdDouble().
-     * 10) Refactor: Encapsulate Fields. Cursor on the declation of "customerId" field. In "Encapsulate Fields" dialog:
-     *    10a) Select only "customerId" field. Don't select "name" field.
-     *    10b) Select all boxes in Encapsulate section: "Get access", "Use accessors even when field is accessible"
-     * 	  10c) Click "Refactor" button.
-     * 9) Refactor: Inline Method getCustomerIdDouble(). Choose option to remove method.
-     * 10) In sameAs() manually replace getCustomerId().getCustomerId() != customerOther.getCustomerId().getCustomerId() with "getCustomerId() != customerOther.getCustomerId()"
+     * 9) Refactor: Rename. customerId1 -> customerId.
+     * 10) Refactor: Inline Method setCustomerId(). Choose option to remove setCustomerId() method - public setters are not welcomed.
+     * 11) Refactor: Rename getCustomerId() to getCustomerIdDouble().
+     * 12) Refactor: Encapsulate Fields. Cursor on the declation of "customerId" field. In "Encapsulate Fields" dialog:
+     *    12a) Select only "customerId" field. Don't select "name" field.
+     *    12b) Select all boxes in Encapsulate section: "Get access", "Use accessors even when field is accessible"
+     * 	  12c) Click "Refactor" button.
+     * 13) Refactor: Inline Method getCustomerIdDouble(). Choose option to remove method.
+     * 14) In sameAs() manually replace getCustomerId().getCustomerId() != customerOther.getCustomerId().getCustomerId() with "getCustomerId() != customerOther.getCustomerId()"
      *
      */
 
