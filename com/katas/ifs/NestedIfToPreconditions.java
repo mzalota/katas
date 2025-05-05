0e06a69180c2e9d042465f8576bd1bc89cc030eb
zal0001m <maxim.zalota@prosiebensat1.com>
2025-05-05 17:27:33 +0200

    Improved function name

--- com/katas/ifs/NestedIfToPreconditions.java
+++ com/katas/ifs/NestedIfToPreconditions.java
@@ -93,6 +93,8 @@ public class NestedIfToPreconditions {
     }
 
 
+    // More realistic example. Mixing all techniques.
+    //
     // 1) Manually add explicit "return;" at the end of the function
     // 2) Intention: "Move up into if statement branches"
     // 3) Intention: "Invert if condition" (code in else condition is much shorter than in if condition, so we bring it to the top of function )
@@ -101,7 +103,7 @@ public class NestedIfToPreconditions {
     // 6) Intention: "Invert if condition"
     // 7) Intention: "Remove redundant else"
     // 8) Intention: "Remove unnecessary 'return'"
-    public void realisticExample(boolean isHungry, boolean foodIsReady) {
+    public void makeSureBabyIsNotHungry(boolean isHungry, boolean foodIsReady) {
         if (isHungry) {
             System.out.println("In if top: she is hungry");
             if (foodIsReady) {
