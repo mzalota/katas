72506d5dd58893fb15bb8789f40e77ea636806f2
zal0001m <maxim.zalota@prosiebensat1.com>
2025-07-13 17:06:34 +0200

    Prepared kata class a bit

---
+++ com/katas/functions/TryCatchDownStack.java
@@ -0,0 +1,46 @@
+package com.katas.functions;
+
+
+public class TryCatchDownStack {
+
+/*
+* Refactoring flow: "Push logic down-stack" and "Pull logic up-stack"
+*
+* ---Push logic "down-stack" (to the function "below")
+* 1) Refactor: "Extract Method": lookupPriceInDBNew() - select last two lines in the method getBaselinePrice().
+* 2) Refactor: "Inline Method...": lookupPriceInDB(). Select first option: "Inline all and remove the method"
+* 3) Refactor: "Rename..." lookupPriceInDBNew() to lookupPriceInDB()
+*
+* --- Pull logic "up-stack" (to the function "above")
+* 1) Refactor: "Extract Method": lookupPriceInDBNew() - content last line in the method lookupPriceInDB().
+* 2) Refactor: "Inline Method...": lookupPriceInDB(). Select first option: "Inline all and remove the method"
+* 3) Refactor: "Rename..." lookupPriceInDBNew() to lookupPriceInDB()
+*
+*/
+
+    public int getBaselinePrice(String priceGroupId, int tarifCategory) {
+        int baselineMonth = 01;
+        int baselineYear = 2001;
+
+        return lookupPriceInDB(priceGroupId, tarifCategory, baselineMonth, baselineYear);
+    }
+
+
+    public int getPrice(String priceGroupId, int tarifCategory, String date) {
+        int month = Integer.parseInt(date.substring(3,5));
+        int year = Integer.parseInt(date.substring(6,10));
+        try {
+            return lookupPriceInDB(priceGroupId, tarifCategory, month, year);
+        } catch (NumberFormatException e) {
+            System.out.println("Error reading from DB");
+            return 0;
+        }
+    }
+
+    private static int lookupPriceInDB(String priceGroupId, int tarifCategory, int month, int year) {
+        int priceGroupIdInt = Integer.parseInt(priceGroupId);
+        //Nonsensical logic below just simulates looking up of a value in DB. It is NOT "domain logic"
+        return priceGroupIdInt * tarifCategory + year / month;
+    }
+
+}
