c0f696f41a017c41cd8e03ed581b830e67c7d610
zal0001m <maxim.zalota@prosiebensat1.com>
2025-07-19 13:00:33 +0200

    In the process (step 3) of writing instructions for kata F05_ReturnInsideForLoop

--- com/katas/functions/F05_ReturnInsideForLoop.java
+++ com/katas/functions/F05_ReturnInsideForLoop.java
@@ -10,11 +10,23 @@ import java.util.List;
  * 1) Intention: "Transform body to single exit-point form". Cursor on extractIndividualLogicOutOfForLoop() method. New variables, result and finished are created.
  * 2) Refactor: "Rename..." variable "finished" to "matchWasFound". Cursor on newly created "finished" variable in extractIndividualLogicOutOfForLoop() method.
  * 3) Refactor: "Rename..." variable "count" to "matchAtElementIndex". Cursor on newly created "count" variable in extractIndividualLogicOutOfForLoop() method.
- * 4) Intention: "Move up into if statement branches". Cursor on "return result;" at the bottom of the extractIndividualLogicOutOfForLoop() method.
- * 5) Intention: "Compute constant value of ''". Cursor on result 1
- * 5) Refactor: "Introduce Constant..." - name DEFAULT_RESULT. Cursor on the magic number "1" in the snippet "int result = 1;"
- * 6) Refactor: "Extract Method": select for loop and all lines above it in extractIndividualLogicOutOfForLoop() method. Name of new variable - "resultCombined"
-
+ * 4) Intention: "Move up into 'if' statement branches". Cursor on "return result;" at the bottom of the extractIndividualLogicOutOfForLoop() method.
+ * 5) Intention: "Compute constant value of 'result'". Cursor on "return result;" in if statement body.
+ * 5) Refactor: "Introduce Constant..." - name DEFAULT_RESULT. Cursor on the magic number "1" in the snippet "return 1;"
+ * 6) Refactor: "Extract Method": select for loop and all lines above it in extractIndividualLogicOutOfForLoop() method. Name of new variable - "resultCombo"
+ *
+ *
+ * -- int getCountOfUnmatched(list)
+ * 3) Refactor: "Introduce Variable": resultFromForLoop - select "resultCombined.result()" in extractIndividualLogicOutOfForLoop() method.
+ * 4) Refactor: "Inline Variable": resultCombo - cursor on "resultCombo" in "resultCombined.result()" snippet. Select "This reference only".
+ * 6) Refactor: "Extract Method": getResultFromForLoop(list) - select "getResult(list).result()" snippet.
+ * 7) Refactor: "Inline Method": getResult() inside getResultFromForLoop() method. Select "Inline this usage only, keep the method" option
+ * 8) Refactor: "Inline Variable": resultCombined. Inside getResultFromForLoop() method.
+ * 9) Manually replace "return new Result(result, matchWasFound, matchAtElementIndex).result()" with "return result;"
+ * 10) Remove line "matchWasFound = true;" inside if statement inside for loop. It is irrelevant for calculating the value of "count" variable.
+ * 11) Intention: "Remove local variable 'matchWasFound'". Cursor on the definition of "matchWasFound" variable in getResultFromForLoop() method.
+ * 12) Move 'return' closer to computation of the value of 'result'. Cursor on "return result;" in getResultFromForLoop() method.
+ *
  * -- int getCountOfUnmatched(list)
  * 3) Refactor: "Introduce Variable": count - select "resultCombined.count()" in extractIndividualLogicOutOfForLoop() method.
  * 4) Refactor: "Inline Variable": resultCombined - cursor on "resultCombined" in "resultCombined.count()" snippet. Select "This reference only".
@@ -22,7 +34,7 @@ import java.util.List;
  * 7) Refactor: "Inline Method": getResult() inside getCount() method. Select "Inline this usage only, keep the method" option
  * 8) Refactor: "Inline Variable": resultCombined. Inside getCount() method.
  * 9) Manually replace "return new Result(result, finished, count).count()" with "return count;"
- * 10) Remove code inside if statement above "break;" in getCount() method. it is irrelevant for calculating the value of "count" variable.
+ * 10) Remove code inside if statement above "break;" in getCount() method. It is irrelevant for calculating the value of "count" variable.
  * 11) Intention: "Remove local variable 'finished'". Cursor on the definition of "finished" variable in getCount() method.
  * 12) Intention: "Remove local variable 'result'". Cursor on the definition of "result" variable in getCount() method.
  *
@@ -38,7 +50,7 @@ public class F05_ReturnInsideForLoop {
         for (Integer element : list) {
             if (element < 0) {
                 System.out.println("Match found at element number:"+count);
-                return getAnInt(count, element);
+                return computeFactor(count, element);
             }
             count++;
         }
@@ -47,7 +59,7 @@ public class F05_ReturnInsideForLoop {
         return 1;
     }
 
-    private int getAnInt(int count, Integer element) {
+    private int computeFactor(int count, Integer element) {
         return element*count;
     }
 }
