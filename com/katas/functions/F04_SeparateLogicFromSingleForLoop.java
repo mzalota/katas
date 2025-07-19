2844e269b2680fa47d4fe1131bedb23fabeff255
zal0001m <maxim.zalota@prosiebensat1.com>
2025-07-19 11:38:06 +0200

    Improved instructions for kata F04_SeparateLogicFromSingleForLoop

--- com/katas/functions/F04_SeparateLogicFromSingleForLoop.java
+++ com/katas/functions/F04_SeparateLogicFromSingleForLoop.java
@@ -4,27 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
 /*
  * Refactoring flow: "Separate logic inside for loop into two separate methods"
  *
  * 1) Refactor: "Extract Method": getResult(list) - for loop and definition of numbers and strings arrays. Result record will be created.
  * 2) Refactor: "Introduce Variable": numbers - select "result.numbers()" in separateTwoAspectsInOneLoop() method.
  * 3) Refactor: "Introduce Variable": strings - select "result.strings()" in separateTwoAspectsInOneLoop() method.
+ * 4) Refactor: "Inline Variable": result - select "result" in "result.numbers()". Select "This reference only".
+ * 5) Refactor: "Inline Variable": result - select "result" in "result.strings()". Result variable disappears.
  *
  * --- getNumbers()
- * 4) Refactor: "Inline Variable": result - select "result" in "result.numbers()". Select "This reference only".
- * 5) Refactor: "Extract Method": getNumbers(list) - select "getResult(list).numbers()" snippet.
- * 6) Refactor: "Inline Method": getResult() inside getNumbers() method. Select "Inline this usage only, keep the method" option
- * 7) Refactor: "Inline Variable": numbers.
- * 8) Refactor: "Inline Variable": result.
- * 9) Manually replace "return new Result(numbers1, strings).numbers();" with "return numbers1;"
+ * 6) Refactor: "Extract Method": getNumbers(list) - select "getResult(list).numbers()" snippet.
+ * 7) Refactor: "Inline Method": getResult() inside getNumbers() method. Select "Inline this usage only, keep the method" option
+ * 8) Refactor: "Inline Variable": result. Inside getNumbers() method.
+ * 9) Manually replace "return new Result(numbers, strings).numbers();" with "return numbers;"
  * 10) Remove code in else statement in getNumbers() method. We don't need strings list in this method.
  * 11) Intention: "Remove local variable 'strings'". Cursor on the definition of strings variable in getNumbers() method.
- * 12) Refactor: "Rename...". Cursor on numbers1 in getNumbers() method.
  *
  * -- getStrings()
- * --repeat steps 4-12, but for strings list.
+ * repeat steps 6-11, but for ArrayList<String> strings variable in method separateTwoAspectsInOneLoop().
+ *
  */
+
 public class F04_SeparateLogicFromSingleForLoop {
 
     public void separateTwoAspectsInOneLoop(List<Integer> list) {
