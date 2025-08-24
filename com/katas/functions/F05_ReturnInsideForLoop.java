782deac5b67ca4ad4c6335ae5aae1225c6325351
zal0001m <maxim.zalota@prosiebensat1.com>
2025-08-24 18:51:01 +0200

    Improved instructions in F05_ReturnInsideForLoop class

--- com/katas/functions/F05_ReturnInsideForLoop.java
+++ com/katas/functions/F05_ReturnInsideForLoop.java
@@ -10,20 +10,25 @@ import java.util.List;
  * 2) Refactor: "Rename..." variable "finished" to "matchWasFound". Cursor on newly created "finished" variable in extractIndividualLogicOutOfForLoop() method.
  * 4) Intention: "Move up into 'if' statement branches". Cursor on "return result;" at the bottom of the extractIndividualLogicOutOfForLoop() method.
  * 5) Intention: "Compute constant value of 'result'". Cursor on "return result;" in if statement body.
- * 5) Refactor: "Introduce Constant..." - name DEFAULT_RESULT. Cursor on the magic number "1" in the snippet "return 1;". Select checkbox "Replace all occurences".
- * 6) Refactor: "Extract Method": select for loop and all lines above it in extractIndividualLogicOutOfForLoop() method. Name of new variable - "resultCombo"
+ * 5) Refactor: "Introduce Constant..." - name DEFAULT_RESULT. Cursor on the magic number "1" in the snippet "return 1;".
+ *   5a) Select checkbox "Replace all occurrences".
+ * 6) Refactor: "Extract Method". Select for loop and all lines above it in extractIndividualLogicOutOfForLoop() method.
+ *   6a) Name new record: Result
+ *   6b) Name of new variable: "resultCombo"
+ *   6c) Name new method: getResult()
  *
  * -- boolean isMatchWasFound(list)
  * A1) Refactor: "Introduce Variable": matchWasFound - select "resultCombo.matchWasFound()" in extractIndividualLogicOutOfForLoop() method.
  * A2) Refactor: "Inline Variable": resultCombo - cursor on "resultCombo" in "resultCombo.matchWasFound()" snippet. Select "This reference only".
  * A3) Refactor: "Extract Method": isMatchWasFound(list) - select "getResult(list).matchWasFound()" snippet.
  * A4) Refactor: "Inline Method": getResult() inside isMatchWasFound() method. Select "Inline this usage only, keep the method" option.
- * A5) Refactor: "Inline Variable": resultCombo.
+ * A5) Refactor: "Inline Variable": resultCombo. Place cursor on resultCombo in isMatchWasFound() method
  * A6) Manually replace "return new Result(result, matchWasFound, matchAtElementIndex).matchWasFound()" with "return matchWasFound;"
  * A7) Manually remove two lines of code inside if statement above "matchWasFound = true;". They are irrelevant for determining if matchWasFound.
  * A8) Intention: "Remove local variable 'result'". Cursor on the definition of "result" variable.
  * A9) Intention: "Remove local variable 'matchAtElementIndex'". Cursor on the definition of "matchAtElementIndex".
  * A10) Intention: "Move 'return' closer to computation of the value of 'matchWasFound'". Cursor on "return matchWasFound;".
+ * A11) Intention: 'Collapse loop with stream 'anyMatch()'. Place the cursor on the for keyword in method isMatchWasFound().
  *
  * -- int getMatchAtElementIndex(list)
  * B1) Refactor: "Introduce Variable": matchAtElementIndex - select "resultCombo.matchAtElementIndex()" in extractIndividualLogicOutOfForLoop() method.
@@ -35,6 +40,7 @@ import java.util.List;
  * B7) Manually remove code inside if statement above "break;". It is irrelevant for calculating the value of the "matchAtElementIndex" variable.
  * B8) Intention: "Remove local variable 'matchWasFound'". Cursor on the definition of "matchWasFound" variable.
  * B9) Intention: "Remove local variable 'result'". Cursor on the definition of "result" variable.
+ * B10) Intention: 'Collapse loop with stream 'count()'. Place the cursor on the for keyword in method getMatchAtElementIndex().
  *
  * -- int getResultFromForLoop(list)
  * C1) Refactor: "Introduce Variable": resultFromForLoop - select "resultCombo.result()" in extractIndividualLogicOutOfForLoop() method.
@@ -46,6 +52,7 @@ import java.util.List;
  * C7) Manually remove line "matchWasFound = true;" inside if statement inside for loop. It is irrelevant for calculating the value of the "result" variable.
  * C6) Intention: "Remove local variable 'matchWasFound'". Cursor on the definition of "matchWasFound" variable.
  * C9) Intention: "Move 'return' closer to computation of the value of 'result'". Cursor on "return result;".
+ *
  * -- Optionally you can try to convert for-each loop to indexed for loop and notice that
  * C10) Intention "Replace for-each loop with indexed 'for' loop". Cursor on 'for' keyword "matchAtElementIndex" can be replaced with "i"
  *
