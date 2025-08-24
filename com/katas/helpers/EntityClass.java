eeaaecf6d38d5b04e26c7f965584e8e5139f6c98
zal0001m <maxim.zalota@prosiebensat1.com>
2025-08-24 14:46:50 +0200

    Improved instructions in F07b_ParamToConstructor class

--- com/katas/helpers/EntityClass.java
+++ com/katas/helpers/EntityClass.java
@@ -9,15 +9,6 @@ public class EntityClass {
         this.multiplier = multi;
     }
 
-    public double getDiscount( final String date, final int orderSize) {
-        final String year = date.substring(6,10);
-        if (year != "2000") {
-            return 15.7 * orderSize * this.multiplier;
-        } else {
-            return 2000;
-        }
-    }
-
     public double getPromotion( final String date, final int orderSize) {
         final String year = date.substring(6,10);
         if (year != "2000") {
