6bfdf9e94dd78e069033f6d0b8461699ad92d757
zal0001m <maxim.zalota@prosiebensat1.com>
2025-09-05 09:45:00 +0200

    Added instructions for the B08_SplitLoop_thru_middle_list kata. need to be cleaned up

--- com/katas/blocks/B08_SplitLoop_thru_middle_list.java
+++ com/katas/blocks/B08_SplitLoop_thru_middle_list.java
@@ -6,22 +6,23 @@ import java.util.List;
 /*
  * Refactoring flow: "Separate logic inside for loop into two separate methods"
  *
- * 1) Refactor: "Extract Method": getResult(list) - for loop and definition of numbers and strings arrays. Result record will be created.
- * 2) Refactor: "Introduce Variable": numbers - select "result.numbers()" in separateTwoAspectsInOneLoop() method.
- * 3) Refactor: "Introduce Variable": strings - select "result.strings()" in separateTwoAspectsInOneLoop() method.
- * 4) Refactor: "Inline Variable": result - select "result" in "result.numbers()". Select "This reference only".
- * 5) Refactor: "Inline Variable": result - select "result" in "result.strings()". Result variable disappears.
- *
- * --- getNumbers()
- * 6) Refactor: "Extract Method": getNumbers(list) - select "getResult(list).numbers()" snippet.
- * 7) Refactor: "Inline Method": getResult() inside getNumbers() method. Select "Inline this usage only, keep the method" option
- * 8) Refactor: "Inline Variable": result. Inside getNumbers() method.
- * 9) Manually replace "return new Result(numbers, strings).numbers();" with "return numbers;"
- * 10) Remove code in else statement in getNumbers() method. We don't need strings list in this method.
- * 11) Intention: "Remove local variable 'strings'". Cursor on the definition of strings variable in getNumbers() method.
- *
- * -- getStrings()
- * repeat steps 6-11, but for ArrayList<String> strings variable in method separateTwoAspectsInOneLoop().
+1) Refactor: Extract method: place the cursor on the System.out.println inside for loop.
+  1a) Name method logicToMove(). Notice that this method takes only one parameter - string
+* 1) Manually add "result.add(string);" above "System.out.println("Even number. Exists in both lists: "+string);" line
+2) Intention: create local variable result. Place the cursor on the "result" variable.
+  2a) Write List<E> as variable type
+3) Intention: Initialize variable 'result'. Place the cursor on the "result" variable. Select "List.of()" option.
+4) Intention: Replace with 'new ArrayList<>()'. Place the cursor on the "List.of()" term.
+5) Intention: Change type of 'result' to List<Object>. Place the cursor on the "List" type declaration of the "List result =" variable.
+6) Manually replace change typ of list from "List<Object>" to List<String>
+7) Manually write "result" below the end of the for loop.
+8) Intention: Bring 'List<String> result' into scope. Place the cursor on the just written "result" word.
+9) Manually move "result = new ArrayList<>();" line up to just below "List<String> result = null;"
+10) Intention: Remove redundant initializer. Place the cursor on "null" term.
+11) Intention: Join declaration and assignment. Place the cursor on result variable declaration.
+12) Intention: Iterate over List<String>. Place the cursor on the result word below for loop.
+  12a) Give the variable in the new for loop the name "string" to be the same as in "result.add(string);" statement.
+13) Manually cut the call to the logicToMove(string) method and paste it into the newly created for loop.
  *
  */
 
@@ -32,7 +33,7 @@ public class B08_SplitLoop_thru_middle_list {
             if (element % 2 == 0) {
                 for (String string : strings) {
                     if (string.equals(Integer.toString(element))) {
-                        System.out.println("Even number. Exists in both lists");
+                        System.out.println("Even number. Exists in both lists: "+string);
                     }
                 }
             }
