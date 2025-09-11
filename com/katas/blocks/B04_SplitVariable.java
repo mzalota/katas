242167f6c74b7df5ca57e6762e9e02d45b8262e2
zal0001m <maxim.zalota@prosiebensat1.com>
2025-09-11 19:50:39 +0200

    Created SplitVariable Kata

---
+++ com/katas/blocks/B04_SplitVariable.java
@@ -0,0 +1,29 @@
+package com.katas.blocks;
+
+public class B04_SplitVariable {
+
+    /*
+     * We want to refactor and extract first if/else into two functions: calulcateIngredientsCosts() and cookingTimeHours()
+     *
+     * 1) Duplicate first if/else block.
+     * 2) In the top "if" block remove lines with the "cookingTimeHours" variables.
+     * 3) In the second "if" block remove lines with the "ingredientsCost" variables.
+     * 4) Intention: Move declaration of 'cookingTimeHours' closer to usages. Cursor on cookingTimeHours definition
+     * 5) Refactor: Extract Method: first if block into "calulcateIngredientsCosts()" function.
+     * 6) Refactor: Extract Method: second if block into "cookingTimeHours()" function.
+     *
+     */
+    public void doingTwoThingsSmaller(boolean isHungry, int defaultValue) {
+        String line = readLibeFromBuffer();
+        String[] headers = line.split(":");
+
+        line = readLibeFromBuffer();
+        String[] values = line.split(":");
+
+        String bottomComment = readLibeFromBuffer();
+    }
+
+    private String readLibeFromBuffer() {
+        return "";
+    }
+}
