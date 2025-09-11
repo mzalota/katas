69f0b83eaf20794e78e1a68aa7f4918836fa881c
zal0001m <maxim.zalota@prosiebensat1.com>
2025-09-11 20:02:46 +0200

    Improved instructions

--- com/katas/blocks/B04_SplitVariable.java
+++ com/katas/blocks/B04_SplitVariable.java
@@ -3,15 +3,12 @@ package com.katas.blocks;
 public class B04_SplitVariable {
 
     /*
-     * We want to refactor and extract first if/else into two functions: calulcateIngredientsCosts() and cookingTimeHours()
+     * Dont reuse line variable. Create separate variable ("split variable") under comment "process second line".
      *
      * 1) Refactor: Introduce Variable. Place the cursor on the "line" variable assignment just below comment "process second line".
      *   1a) Name the variable "line" - exactly the same name.
      * 2) Refactor: Rename Variable: new name "secondLine". Place the cursor on the newly created "line" variable.
-     * 3) Intention: remove redundant Initializer. Place cursor on the "line" variable that is underlined in red
-     * 4) Intention: Move declaration of 'cookingTimeHours' closer to usages. Cursor on cookingTimeHours definition
-     * 5) Refactor: Extract Method: first if block into "calulcateIngredientsCosts()" function.
-     * 6) Refactor: Extract Method: second if block into "cookingTimeHours()" function.
+     * 3) Intention: Remove redundant initializer. Place cursor on the "secondLine" variable that is underlined in red.
      *
      */
     public void doingTwoThingsSmaller(boolean isHungry, int defaultValue) {
